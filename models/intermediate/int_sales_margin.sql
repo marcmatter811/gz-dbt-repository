541664cf14f6ad39ec12608f62e18f7c92a00669
Marc Matter <marc.matter1@gmail.com>
2025-02-18 15:11:47 +0000

    commit

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,14 +1,14 @@
 SELECT
     date_date,
     orders_id,
-    products_id,
+    sales.products_id,
     quantity,
     revenue,
     purchase_price*quantity AS purchase_cost,
     revenue-(purchase_price*quantity) AS margin
     FROM 
-        {{ ref('stg_raw__sales') }}
+        {{ ref('stg_raw__sales') }} AS sales
     LEFT JOIN 
-        {{ ref('stg_raw__product') }}
-        ON stg_raw__sales.products_id = stg_raw__product.products_id
+        {{ ref('stg_raw__product') }} AS product
+        ON sales.products_id = product.products_id
 
